74c6a0c57b65a0b5eb373d30a39b8d1b67e7a99e
David Ockey <crash_override@att.net>
2015-11-29 17:11:21 -0700

    Refactored to use less Analog Inputs
    Refactored and redesigned the code.  Using multiple Analog Inputs with
    the current ATTiny Bootloader was proving to be problematic.  There are
    now only a limited number of time settings available through the button
    mode switcher.

--- Super_Night_Light_Trinket.ino
+++ Super_Night_Light_Trinket.ino
@@ -8,10 +8,8 @@
 #include <EEPROM.h>
 
 #define CdS 2
-#define BUTTON 1
-#define KNOB 4
-#define KNOBPOWER 3
-#define LED 0
+#define BUTTON 3
+#define LED 1
 
 // LED Modes
 #define OFF 0
@@ -22,10 +20,14 @@
 // Operation Modes
 #define SHORT 0
 #define LONG 1
+// Operation Mode Times (in miliseconds)
+#define SHORT_TIME 15000
+#define LONG_TIME 60000
 
 unsigned int threshold = 150;
 
-boolean lightSensed;
+bool lightSensed;
+bool triggered;
 
 byte LEDMode;
 byte LEDBrightness;
@@ -36,17 +38,16 @@ unsigned long endTime;
 void setup() {
   pinMode(BUTTON, INPUT_PULLUP);
   pinMode(CdS, INPUT);
-  pinMode(KNOBPOWER, OUTPUT);
-  pinMode(KNOB, INPUT);
   pinMode(LED, OUTPUT);
 
-  digitalWrite(KNOBPOWER, HIGH);
   digitalWrite(LED, LOW);
 
   LEDMode = OFF;
   LEDBrightness = 0;
   startTime = 0;
   endTime = 0;
+
+  triggered = false;
 
   // Retreive previous setting, and set to default if no value was set
   EEPROM.get(0x0, operationMode);
@@ -57,37 +58,41 @@ void setup() {
 
 void manualControl() {
   startTime = millis();
-  for (byte i = 0; i <= 255; i++) {
+  for (byte i = 0; i < 255; i++) {
     analogWrite(LED, i);
-    delay(1);
+    delay(2);
   }
-  endTime = startTime + 3000;  
+  digitalWrite(LED, HIGH);
+  endTime = startTime + 3000;
   while (digitalRead(BUTTON) == LOW && ((long)(millis() - endTime) < 0));
   if (((long)(millis() - endTime) >= 0)) {
     operationMode = (operationMode + 1) % 2;
-    EEPROM.update(0x0, operationMode);
+    EEPROM.write(0x0, operationMode);
     digitalWrite(LED, LOW);
-    delay(250);
+    delay(300);
     digitalWrite(LED, HIGH);
-    delay(250);
+    delay(100);
     digitalWrite(LED, LOW);
-    delay(250);
+    delay(100);
     digitalWrite(LED, HIGH);
-    delay(250);
+    delay(100);
     digitalWrite(LED, LOW);
-    delay(250);
-    digitalWrite(LED, HIGH);
-    delay(250);
-    digitalWrite(LED, LOW);
+    if (operationMode == LONG) {
+      delay(100);
+      digitalWrite(LED, HIGH);
+      delay(100);
+      digitalWrite(LED, LOW);
+    }
     // Wait for the user to release the button and then account for debounce before moving on.
     while (digitalRead(BUTTON) == LOW);
     delay(100);
   } else {
     while (digitalRead(BUTTON) == HIGH);
-    for (byte i = 255; i >= 0; i--) {
+    for (short i = 255; i > 0; i--) {
       analogWrite(LED, i);
-      delay(4);
+      delay(2);
     }
+    digitalWrite(LED, LOW);
   }
 }
 
@@ -97,41 +102,43 @@ void loop() {
   }
 
   lightSensed = (analogRead(CdS) <= threshold);
+//  lightSensed = false;
 
   switch (LEDMode) {
     case OFF:
-      if (!lightSensed) {
+      if (!lightSensed && !triggered) {
         LEDMode = FADINGON;
+        triggered = true;
+      } else if (lightSensed && triggered) {
+        triggered = false;
       }
       break;
     case FADINGON:
       if (lightSensed) {
-        LEDMode = OFF;
+        LEDMode = FADINGOFF;
         LEDBrightness = 0;
       } else {
         LEDBrightness++;
         if (LEDBrightness == 255) {
           LEDMode = ON;
           startTime = millis();
+
+          // Checks to see what operating mode the device is in.
+          if (operationMode == SHORT) {
+            endTime = startTime + SHORT_TIME;
+          } else if (operationMode == LONG) {
+            endTime = startTime + LONG_TIME;
+          } else {
+            endTime = startTime + 1;
+          }
         }
         analogWrite(LED, LEDBrightness);
-        delay(1);
+        delay(2);
       }
       break;
     case ON:
-      // Checks to see what operating mode the device is in.
-      // SHORT -> 10 Seconds to 1 Minute
-      // LONG  -> 1 Minute to 1 hour
-      if (operationMode == SHORT) {
-        endTime = startTime + (map(analogRead(KNOB), 0 , 1024 , 10, 60) * 1000);
-      } else if (operationMode == LONG) {
-        endTime = startTime + (map(analogRead(KNOB), 0 , 1024 , 60, 360) * 1000);
-      } else {
-        endTime = startTime + 1;
-      }
-      
-      // This type-casting math accounts for the 49-day rollover      
-      if ((long)(millis() - endTime) >= 0) {
+      // This type-casting math accounts for the 49-day rollover
+      if (lightSensed || (long)(millis() - endTime) >= 0) {
         LEDMode = FADINGOFF;
       }
       break;
@@ -144,7 +151,7 @@ void loop() {
         if (LEDBrightness == 0) {
           LEDMode = OFF;
         }
-        delay(1);
+        delay(4);
       }
       analogWrite(LED, LEDBrightness);
       break;
